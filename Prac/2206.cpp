2d74cb6039f735421e25a128d91bdf58fc2b3b9b
KimYoungWoo <120362572+KimuYounguWoo@users.noreply.github.com>
2023-03-08 20:21:45 +0900

    Create 2206.cpp

---
+++ Prac/2206.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define x first
+#define y second
+
+
+char n, m;
+vector<pair<char, char>> trans;
+
+int ccc[5][5][2][2];
+// 인덱스 [i]의 {X or Y}에서 올바른 문자열을 맞추기위해 인덱스 [j] {X or Y}와 교환한 횟수 저장
+
+bool checkss(vector<pair<char, char>> vs) {
+    int sizes = trans.size();
+    bool ddd;
+    for (int i=0; i<sizes; i++) {
+        if (vs[i].x == vs[i].y) {
+            ddd = true;}
+        else {
+            ddd = false;
+        }
+    }
+    return ddd;
+}
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int cnt = 0;
+    for (int i=0; i<4; i++) {
+        string tmp;
+        cin >> tmp;
+        trans.push_back({tmp[0], tmp[1]});
+    }
+    for (int i=0; i<4; i++) {
+        cout << trans[i].x << " " << trans[i].y << '\n';
+    }
+    int sizes = trans.size();
+    for (int i=0; i<sizes; i++) {
+        if(trans[i].x != trans[i].y) {
+            for (int j=i; j<sizes; j++) {
+                if (trans[i].x == trans[j].x) {
+                    ccc[i][j][0][0] += j-i;
+                    swap(trans[i].x, trans[j].x);
+                    cout << "1 test= " << ccc[i][j][0][0] << '\n';
+                }
+                if (trans[i].x == trans[j].y) {
+                    ccc[i][j][0][1] += j-i;
+                    swap(trans[i].x, trans[j].y);
+                    cout << "2 test= " << ccc[i][j][0][1] << '\n';
+                }
+                if (trans[i].y == trans[j].x) {
+                    ccc[i][j][1][0] += j-i;
+                    swap(trans[i].y, trans[j].x);
+                    cout << "3 test=  " << ccc[i][j][1][0] << '\n';
+                    }
+                if (trans[i].y == trans[j].y) {
+                    ccc[i][j][1][1] += j-i;
+                    swap(trans[i].y, trans[j].y);
+                    cout << "4 test=  " << ccc[i][j][1][1] << '\n';
+                    }
+                cnt += min({ccc[i][j][0][0], ccc[i][j][0][1], ccc[i][j][1][0], ccc[i][j][1][1]});
+            }
+        }
+    }    
+            for (int i=0; i<4; i++) {
+        cout << trans[i].x << " " << trans[i].y << '\n';
+    }
+}
